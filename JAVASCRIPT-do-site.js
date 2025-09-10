6ce9aee15e3a25b013a277dad563198c99cf7505
G4stavos2ntos <161878573+G4stavos2ntos@users.noreply.github.com>
2025-09-10 15:58:15 -0300

    Update JAVASCRIPT-do-site.js

--- JAVASCRIPT-do-site.js
+++ JAVASCRIPT-do-site.js
@@ -54,33 +54,100 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Arrays de pontos
   const pontosReciclagem = [
+    
     {
-      lat: -23.49903,
-      lng: -46.798947,
-      nome: "Praça Pastor José Maria da Silva",
-      endereco:
-        "R. Pastor José Maria da Silva, 33 - Jardim Elvira, Osasco - SP",
+      lat:-23.508101, 
+      lng:-46.784338,
+      nome:"Ecoponto Jardim Mutinga",
+      endereco:"Avenida Ônix, nº 783, Jardim Ayrosa, Osasco/SP",
+      CEP:"06280-030"
+    },
+
+
+    {
+      lat:-23.564634,
+      lng:-46.794282,
+      nome:" Ecoponto Novo Osasco",
+      endereco:" Rua Theodoro de Souza Brandão, nº 1020, Novo Osasco, Osasco/SP",
+      CEP:"06045-150"
     },
     {
-      lat: -23.4993286,
-      lng: -46.8152642,
-      nome: "EcoPonto Jardim Elvira",
-      endereco: "Av. João de Andrade, 500 - Jardim Elvira, Osasco - SP",
+      
+      lat:-23.498848,
+      lng: -46.784705,
+      nome:"Ecoponto Baronesa",
+      endereco:" Rua Duke Elington, nº 360, ao lado da Academia de Lutas, Osasco/SP",
+      CEP:"06268-140"
     },
+    {
+     
+      lat: -23.540736,
+      lng:-46.801626,
+      nome:"Ecoponto Vila Pestana",
+      endereco:" Rua José Antônio Augusto, nº 55, Osasco/SP",
+      CEP:"06170-170"
+    },
+    {
+     
+      lat:-23.499121,
+      lng:-46.799076,
+      nome:"Ecoponto Helena Maria",
+      endereco:"Avenida Walt Disney, esquina com Rua Belmiro Alves da Silva, Jardim Elvira, Osasco/SP",
+      CEP:"06253-060"
+    },
+    {
+
+
+      lat:-23.560730,
+      lng:-46.788969,
+      nome:"Ecoponto Jaguaribe",
+      endereco:"Rua Fernando Miolin Filho, nº 150, Jaguaribe, Osasco/SP",
+      CEP:"06050-330"
+    },
+    {
+   
+      lat:-23.570530,
+      lng:-46.807022,
+      nome:"Ecoponto Veloso",
+      endereco:"Rua Dr. Armando Anjo Corrêa Filho, nº 195 (esquina com Rua Alberto Lameu), Osasco/SP",
+      CEP:"06150-500"
+    },
+    {
+      lat:-23.540979,
+      lng:-46.809095,
+      nome:"Ecoponto Cidade das Flores",
+      endereco:"Avenida Ipê, nº 527, ao lado da CEMEI Zaíra Colino, Osasco/SP",
+      CEP:"06184-170"
+    },
+
+    
   ];
 
   const pontosDoacao = [
     {
-      lat: -23.510123,
-      lng: -46.800456,
-      nome: "ONG Exemplo 1",
-      endereco: "R. Exemplo, 100 - Osasco, SP",
+
+
+      lat:-23.534632 ,
+      lng:-46.774939 ,
+      nome: "Fundo Social de Solidariedade – Prefeitura de Osasco",
+      endereco: "Avenida Lázaro de Mello Brandão, 140 – Vila Campesina, Osasco/SP",
+      telefone:"(11) 3652-9400"
     },
     {
-      lat: -23.515789,
-      lng: -46.810123,
-      nome: "ONG Exemplo 2",
-      endereco: "Av. Exemplo, 200 - Osasco, SP",
+     
+      lat:-23.501530 ,
+      lng:-46.774911 ,
+      nome: "Bazar Beneficente Mercatudo – Casas André Luiz (Osasco)",
+      endereco: " Avenida Presidente Médice, 1365 – Jardim Mutinga, Osasco/SP",
+      telefones:"(11) 2459-7000 / (11) 2457-7733 / (11) 95427-3700 "
+    },
+    {
+      
+      lat:-23.521151 ,
+      lng:-46.780386 ,
+      nome: "Lar “Jesus entre as Crianças”",
+      endereco: "Rua João Kaufmann, 437 – Rochdale, Osasco/SP – CEP 06220-060",
+      telefone:"(11) 3687-8261"
     },
   ];
 
