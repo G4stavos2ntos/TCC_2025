b04a0a97443fee469589e7d46cf12ae3e361cbc3
G4stavos2ntos <161878573+G4stavos2ntos@users.noreply.github.com>
2025-10-06 13:21:33 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,21 @@
 <?php
-$host = "127.0.0.1";   // ou localhost
-$porta = "3306";       // porta do MySQL
-$usuario = "root";     // usuário do MySQL
-$senha = "1234";  // senha do MySQL
-$banco = "tcc-ecosolidario"; // banco que você já criou
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$host = "127.0.0.1";
+$porta = "3306";
+$usuario = "root";
+$senha = "1234";
+$banco = "tcc-ecosolidario";
 
 $conn = new mysqli($host, $usuario, $senha, $banco, $porta);
 
 if ($conn->connect_error) {
-    die("Erro ao conectar: " . $conn->connect_error);
-} else {
-    echo "Conexão bem-sucedida!";
+    die("Erro na conexão: " . $conn->connect_error);
 }
+echo "!!!!BANCO CONECTADO COM SUCESSO!!!!!";
 ?>
